068215d316ad97325758be1176937c1177947bd1
Matt Mulligan <matthew.mulligan93@gmail.com>
2017-01-07 10:24:47 +1100

    Matt Mulligan Programing Assignment 2 Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this solution contains two high level functions
+## the first fucntion is used to set and retieve the orginal and inverse matrixes
+## the second function is used to check for cached IM values and generate fresh IM values if cache is null
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## this function contain contains a list of four functions that;
+## set the orignal matirx (setOM)
+## Return the original matrix (getOM)
+## stores the inverse matrix passed to it (setIM)
+## returns the inverse matrix (getIM)
+makeCacheMatrix <- function(x = matrix()) 
+{
+      IM <- NULL
+      setOM <- function(y = matrix())
+      {
+            x <<- y
+            IM <<- NULL
+      }
+      getOM <- function() 
+      {
+            x
+      }
+      setIM <- function(inverse_matrix) 
+      {
+            IM <<- inverse_matrix
+      }
+      getIM <- function()
+      {
+            IM
+      }
+      list(setOM = setOM, getOM = getOM, setIM = setIM, getIM = getIM)
+}
 
+## this function takes an input of makeCacheMatrix, checks to see if the inverse matrix (IM) is null
+## if null, to uses solve to invert it, if not null, it returns the current IM as the cached value
+cacheSolve <- function(x, ...) 
+{
+      IM <- x$getIM()
+      if(!is.null(IM))
+      {
+            message("Matrix already cached, returning cached inverse")
+            return(IM)
+      }
+      OM <- x$getOM()
+      IM <- solve(OM)
+      x$setIM(IM)
+      IM
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
